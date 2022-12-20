fb2d2e0ac7afd8fccb94727d6bbfba2ef6cb40ed
Roman Chistiakov <r.chistiakov@wanna.fashion>
2022-12-20 14:43:09 +0300

    fix

--- script.sh
+++ script.sh
@@ -3,4 +3,6 @@
 echo "Trying to read ${INPUT_PATH}/package.json"
 PACKAGE_VERSION=$(cat ${INPUT_PATH}/package.json | jq '.version' | tr -d '"')
 
+echo "Found version: ${INPUT_PATH}"
+
 echo "package-version=${INPUT_PATH}" >> $GITHUB_OUTPUT
